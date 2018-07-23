5bed09aba9e868a3db5702cab09bf9266fcf9747
wuwm <wwm1994@gmail.com>
2018-07-23 10:23:09 +0800

    Initial Commit

---
+++ VCRunningLocationLogger/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VCRunningLocationLogger
+//
+//  Created by Weiming Wu on 2018/7/23.
+//  Copyright © 2018 Weiming Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VCRunningLocationLogger/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VCRunningLocationLogger
+//
+//  Created by Weiming Wu on 2018/7/23.
+//  Copyright © 2018 Weiming Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
